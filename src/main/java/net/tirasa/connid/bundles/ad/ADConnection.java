3dc50d67f725fe4c4490b841e59a1fac52ad32d5
fmartelli <fabio.martelli@gmail.com>
2015-04-30 10:23:40 +0200

    [AD-44] fixes

--- src/main/java/net/tirasa/connid/bundles/ad/ADConnection.java
+++ src/main/java/net/tirasa/connid/bundles/ad/ADConnection.java
@@ -28,7 +28,7 @@ import javax.naming.directory.Attributes;
 import javax.naming.ldap.Control;
 import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapContext;
-import net.tirasa.adsddl.ntsd.data.SDFlagsControl;
+import net.tirasa.adsddl.ntsd.controls.SDFlagsControl;
 import net.tirasa.connid.bundles.ad.schema.ADSchema;
 import net.tirasa.connid.bundles.ad.util.TrustAllSocketFactory;
 import net.tirasa.connid.bundles.ldap.LdapConnection;
